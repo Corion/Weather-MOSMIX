04c1d36a6b3aa3fa760504a4ab7eb0ba97d95550
Max Maischein <corion@corion.net>
2019-07-15 21:29:21 +0200

    Add placeholder file for the version and name

---
+++ lib/Weather/MOSMIX.pm
@@ -0,0 +1,18 @@
+package Weather::MOSMIX;
+use strict;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Reader for MOSMIX weather forecast files
+
+=head1 SEE ALSO
+
+L<https://opendata.dwd.de/weather/>
+
+L<https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_S/all_stations/kml/>
+
+=cut
+
+1;
