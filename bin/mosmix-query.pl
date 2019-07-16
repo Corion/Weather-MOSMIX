6ea759d44680fcb3506622b45d3c1b3e4867abc5
Max Maischein <corion@corion.net>
2019-07-16 20:52:30 +0200

    Add Weather::MOSMIX::Writer for writing to an SQLite DB
    This is still very SQLite specfic and not easily overridable
    currently. But then again, it will most likely only store in an
    SQLite database anyway

--- bin/mosmix-query.pl
+++ bin/mosmix-query.pl
@@ -1,3 +1,112 @@
+package Weather::MOSMIX::Writer;
+use strict;
+use Moo 2;
+use feature 'signatures';
+no warnings 'experimental::signatures';
+use DBI;
+require POSIX;
+use JSON;
+
+# This should be MooX::Role::DBConnection
+has 'dsn' => (
+    is => 'ro',
+);
+
+has 'user' => (
+    is => 'ro',
+);
+has 'password' => (
+    is => 'ro',
+);
+has 'options' => (
+    is => 'lazy',
+    default => sub { {
+		#AutoCommit => 0,
+	} },
+);
+
+has 'dbh' => (
+    is => 'lazy',
+    default => \&_connect_db,
+);
+
+has 'insert_location_sth' => (
+    is => 'lazy',
+    default => \&_prepare_location_sth,
+);
+
+has 'insert_forecast_sth' => (
+    is => 'lazy',
+    default => \&_prepare_forecast_sth,
+);
+
+has 'json' => (
+    is => 'lazy',
+    default => sub {
+		JSON->new()
+	},
+);
+
+sub _connect_db( $self ) {
+	my $dbh = DBI->connect(
+	    $self->dsn,
+	    $self->user,
+	    $self->password,
+	    $self->options,
+	);
+	$dbh->do('PRAGMA synchronous = OFF');
+	$dbh->do('PRAGMA journal_mode = MEMORY');
+	$dbh->{AutoCommit} = 0;
+	$dbh
+}
+
+sub _prepare_location_sth( $self ) {
+	$self->dbh->prepare(<<'SQL');
+	    insert or replace into forecast_location (
+	        name, description, latitude, longitude, elevation
+	    ) values (
+	        ?,    ?,           ?,        ?,         ?
+	    )
+SQL
+}
+
+sub _prepare_forecast_sth( $self ) {
+	$self->dbh->prepare(<<'SQL');
+	    insert into forecast (
+	        name, forecasts, expiry
+	    ) values (
+	        ?,    ?,        ?
+	    )
+SQL
+}
+
+sub purge_expired_records( $self, $date = POSIX::strftime('%Y-%m-%d %H:%M:%S', gmtime()) ) {
+	$self->dbh->do(<<'SQL', $date);
+	    delete from forecast
+	        where expiry <= ?
+SQL
+}
+
+sub insert( $self, $expiry, @records ) {
+	my $i = 0;
+	$self->insert_location_sth->execute_for_fetch(sub {
+		my $rec = $records[$i++];
+		return if ! $rec;
+		[@{$rec}{qw(name description latitude longitude elevation)}]
+	});
+	$i = 0;
+	$self->insert_forecast_sth->execute_for_fetch(sub {
+		my $rec = $records[$i++];
+		return if ! $rec;
+		my $f = $self->json->encode( $rec->{forecasts} );
+		[$rec->{name}, $f, $expiry]
+	});
+};
+
+sub commit( $self ) {
+	$self->dbh->commit;
+}
+
 package Weather::MOSMIX::Reader;
 use strict;
 use Moo 2;
