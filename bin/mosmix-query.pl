9add899d747829cc292f10db52222fdc8259ea5c
Max Maischein <corion@corion.net>
2019-07-21 15:43:21 +0200

    First attempt at a not-so unusable command line tool

--- bin/mosmix-query.pl
+++ bin/mosmix-query.pl
@@ -5,5 +5,27 @@ use Weather::MOSMIX;
 my $w = Weather::MOSMIX->new(
     dsn => 'dbi:SQLite:dbname=db/forecast.sqlite',
 );
-print Dumper
+my $f =
     $w->forecast(latitude => 50.11, longitude => 8.68 );
+# calculate min-temp, max-temp, primary weather for the period
+# resp. go from hourly to four-hour windows?!
+my ($min, $max) = (1000,0);
+my $loc = $f->{description};
+(my $temp) = grep{ $_->{type} eq 'TTT' } @{ $f->{forecasts}};
+
+# Restrict to relevant slice here, instead of taking all
+for my $f (grep { length $_ } @{ $temp->{values} }) {
+    if( $f < $min ) {
+        $min = $f;
+    };
+    if( $f > $max ) {
+        $max = $f;
+    };
+};
+
+my $weather = '-';
+
+$max -= 274.1;
+$min -= 274.1;
+
+print "$loc ($min/$max) $weather\n";
