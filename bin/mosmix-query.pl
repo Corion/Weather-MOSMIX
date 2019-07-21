3e2fa9627d2448ca19ff4dc8d040589bd58a47ec
Max Maischein <corion@corion.net>
2019-07-21 21:56:04 +0200

    Note the config cascade we might want

--- bin/mosmix-query.pl
+++ bin/mosmix-query.pl
@@ -5,5 +5,11 @@ use Data::Dumper;
 my $w = Weather::MOSMIX->new(
     dsn => 'dbi:SQLite:dbname=db/forecast.sqlite',
 );
+
+# Read location from .locationrc
+# File::HomeDir
+# ~/.config/.locationrc
+# ~/.locationrc
+
 print Dumper
     $w->forecast(latitude => 50.11, longitude => 8.68 );
