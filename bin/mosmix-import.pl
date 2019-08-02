53eb6e5d5d343e4b784c827f004f592cff7254b2
Max Maischein <corion@corion.net>
2019-08-02 15:03:27 +0200

    Optionally fetch the latest forecast file

--- bin/mosmix-import.pl
+++ bin/mosmix-import.pl
@@ -4,10 +4,70 @@ use warnings;
 use Weather::MOSMIX::Writer;
 use Weather::MOSMIX::Reader;
 
-my $w = Weather::MOSMIX::Writer->new(
-    dsn => 'dbi:SQLite:dbname=db/forecast.sqlite',
+use HTTP::Tiny;
+use File::Temp 'tempfile';
+
+use Getopt::Long;
+
+our $VERSION = '0.01';
+
+GetOptions(
+    'import' => \my $import,
+    'fetch'  => \my $fetch,
+    'verbose' => \my $verbose,
 );
-my $r = Weather::MOSMIX::Reader->new(
-    writer => $w,
-);
-$r->read_zip( $ARGV[0] );
+
+sub status {
+    if( $verbose ) {
+        print "@_\n";
+    };
+};
+
+my %actions;
+
+if( !@ARGV) {
+    $fetch = 1;
+} else {
+    $import = 1;
+};
+
+warn "$fetch / $import";
+if( ! ($import || $fetch )) {
+    $fetch = 1;
+    $import = 1;
+};
+$actions{ import } = $import;
+$actions{ fetch  } = $fetch;
+
+my @files = @ARGV;
+
+if( $actions{ fetch }) {
+    my $base = 'https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_S/all_stations/kml/MOSMIX_S_LATEST_240.kmz';
+    status( "Fetching $base" );
+
+    my $ua = HTTP::Tiny->new();
+    my( $fh, $name ) = tempfile();
+    close $fh;
+
+    my $res = $ua->mirror($base => $name);
+
+    if( ! $res->{success}) {
+        die $res->{message};
+    };
+
+    push @files, $name;
+};
+
+if( $actions{ import }) {
+    my $w = Weather::MOSMIX::Writer->new(
+        dsn => 'dbi:SQLite:dbname=db/forecast.sqlite',
+    );
+    my $r = Weather::MOSMIX::Reader->new(
+        writer => $w,
+    );
+
+    for my $file (@files) {
+        status("Importing $file\n");
+        $r->read_zip( $file );
+    };
+};
