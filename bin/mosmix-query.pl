3ad334d77898a5e17278cd24bbcd4ac200b25d52
Max Maischein <corion@corion.net>
2020-01-02 14:42:19 +0100

    Add the date for when the weather applies

--- bin/mosmix-query.pl
+++ bin/mosmix-query.pl
@@ -22,7 +22,9 @@ my $out =
 binmode STDOUT, ':encoding(UTF-8)';
 
 for my $day ('today', 'tomorrow') {
-    print "$day $out->{location} (\x{1F321}$Weather::MOSMIX::Weathercodes::as_emoji $out->{weather}->{$day}->{min}/$out->{weather}->{$day}->{max})\n";
+    my $issue = substr $out->{issuetime},0,10;
+    my $date = $out->{weather}->{$day}->{date}->strftime('%Y-%m-%d');
+    print "$out->{location} (\x{1F321}$Weather::MOSMIX::Weathercodes::as_emoji $out->{weather}->{$day}->{min}/$out->{weather}->{$day}->{max}) ($date)\n";
     for my $w (@{ $out->{weather}->{$day}->{weather}}) {
         print $w->{timestamp}, "\n";
         print sprintf "%02d %s$Weather::MOSMIX::Weathercodes::as_emoji %s\n", $w->{timestamp}->hour, $w->{description}->{emoji}, $w->{description}->{text};
