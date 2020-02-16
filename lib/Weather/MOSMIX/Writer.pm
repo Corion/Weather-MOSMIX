8b546e12c8a0e4c43b771fd3562064fed924c736
Max Maischein <corion@corion.net>
2020-02-16 12:40:10 +0100

    Convert tabs to spaces

--- lib/Weather/MOSMIX/Writer.pm
+++ lib/Weather/MOSMIX/Writer.pm
@@ -46,41 +46,41 @@ has 'insert_forecast_sth' => (
 has 'json' => (
     is => 'lazy',
     default => sub {
-		JSON->new()
-	},
+        JSON->new()
+    },
 );
 
 sub _prepare_location_sth( $self ) {
-	$self->dbh->prepare(<<'SQL');
-	    insert or replace into forecast_location (
-	        name, description, latitude, longitude, elevation
-	    ) values (
-	        ?,    ?,           ?,        ?,         ?
-	    )
+    $self->dbh->prepare(<<'SQL');
+        insert or replace into forecast_location (
+            name, description, latitude, longitude, elevation
+        ) values (
+            ?,    ?,           ?,        ?,         ?
+        )
 SQL
 }
 
 sub _prepare_forecast_sth( $self ) {
-	$self->dbh->prepare(<<'SQL');
-	    insert into forecast (
-	        name, forecasts, expiry, issuetime
-	    ) values (
-	        ?,    ?,        ?,       ?
-	    )
+    $self->dbh->prepare(<<'SQL');
+        insert into forecast (
+            name, forecasts, expiry, issuetime
+        ) values (
+            ?,    ?,        ?,       ?
+        )
 SQL
 }
 
 sub purge_expired_records( $self, $date = POSIX::strftime('%Y-%m-%d %H:%M:%SZ', gmtime()) ) {
-	$self->dbh->do(<<'SQL', $date);
-	    delete from forecast
-	        where expiry <= ?
+    $self->dbh->do(<<'SQL', $date);
+        delete from forecast
+            where expiry <= ?
 SQL
 }
 
 sub purge_outdated_expired_records( $self ) {
-	$self->dbh->do(<<'SQL');
-	    delete from forecast
-	        where expiry < (select max(expiry) from forecast)
+    $self->dbh->do(<<'SQL');
+        delete from forecast
+            where expiry < (select max(expiry) from forecast)
 SQL
 }
 
@@ -94,23 +94,25 @@ sub start( $self ) {
 };
 
 sub insert( $self, $expiry, @records ) {
-	my $i = 0;
-	$self->insert_location_sth->execute_for_fetch(sub {
-		my $rec = $records[$i++];
-		return if ! $rec;
-		[@{$rec}{qw(name description latitude longitude elevation)}]
-	});
-	$i = 0;
-	$self->insert_forecast_sth->execute_for_fetch(sub {
-		my $rec = $records[$i++];
-		return if ! $rec;
-		my $f = $self->json->encode( $rec->{forecasts} );
-		[$rec->{name}, $f, $expiry, $rec->{issuetime}]
-	});
+    my $i = 0;
+    $self->insert_location_sth->execute_for_fetch(sub {
+        my $rec = $records[$i++];
+        return if ! $rec;
+        [@{$rec}{qw(name description latitude longitude elevation)}]
+    });
+    $i = 0;
+    $self->insert_forecast_sth->execute_for_fetch(sub {
+        my $rec = $records[$i++];
+        return if ! $rec;
+        my $f = $self->json->encode( $rec->{forecasts} );
+        [$rec->{name}, $f, $expiry, $rec->{issuetime}]
+    });
 };
 
 sub commit( $self ) {
-	$self->dbh->commit;
+    $self->dbh->commit;
+}
+
 =head2 C<< Weather::MOSMIX::Writer->create_db >>
 
     $w->create_db(
